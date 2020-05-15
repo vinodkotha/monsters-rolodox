c4a22eec7f3936864c45f163e2c94413c7f5c6e1
vinodkotha <vennivinod@gmail.com>
2020-05-14 23:09:04 -0400

    Updates

---
+++ precache-manifest.b8b2ba4b3bc0d1769e3694688d9f3181.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "143031a0c3348fdc40d36598ee0e5067",
+    "url": "/monsters-rolodox/index.html"
+  },
+  {
+    "revision": "8f56b8e6cc89de381e2c",
+    "url": "/monsters-rolodox/static/css/main.046e4dc8.chunk.css"
+  },
+  {
+    "revision": "f7b84cd810271619eca9",
+    "url": "/monsters-rolodox/static/js/2.7d94dc6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodox/static/js/2.7d94dc6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f56b8e6cc89de381e2c",
+    "url": "/monsters-rolodox/static/js/main.69c5cdbe.chunk.js"
+  },
+  {
+    "revision": "c345a9fba7ae2e5fb61b",
+    "url": "/monsters-rolodox/static/js/runtime-main.71042868.js"
+  }
+]);
